6f748df9dbee6a3ee787494f868ff0c87caaf65c
kalu4136b <kalharalakshitha25@gmail.com>
2025-10-16 23:50:01 +0530

    task dto

--- Lms/src/main/java/com/lms/lms/DTO/TaskDTO.java
+++ Lms/src/main/java/com/lms/lms/DTO/TaskDTO.java
@@ -1,4 +1,29 @@
 package com.lms.lms.DTO;
 
-public class TaskDTO {
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "tasks")
+public class TaskEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int duration;     // in minutes or hours — clarify in UI
+    private int priority;     // lower number = higher priority
+    private LocalDateTime startTime;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "task_dependencies",
+            joinColumns = @JoinColumn(name = "task_id")
+    )
+    @Column(name = "dependency_id")
+    private List<Long>dependencies;
 }
